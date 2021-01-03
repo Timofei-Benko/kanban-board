b181af45472250f30effb6d351be3288a306b82b
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-01-03 21:15:13 +0300

    refactor: move modal display functional to separate module

--- source/js/addCard.js
+++ source/js/addCard.js
@@ -4,39 +4,23 @@
 
 import { variables } from './variables.js';
 
-const addCardBtn = document.body.querySelector('.board__add-new-btn'),
-      toDoColumn = document.body.querySelector('div[data-column="toDo"]'),
-      modal = document.body.querySelector('.add-card-modal'),
-      titleInput = modal.querySelector('input[data-input_type="title"]'),
-      descriptionInput = modal.querySelector('textarea[data-input_type="description"]');
+const toDoColumn = document.body.querySelector('div[data-column="toDo"]'),
+      modal = document.body.querySelector('#add-card-modal'),
+      saveBtn = modal.querySelector('.modal__save-btn'),
+      titleInput = modal.querySelector('#add-card-modal input[data-input_type="title"]'),
+      descriptionInput = modal.querySelector('#add-card-modal textarea[data-input_type="description"]');
 
 
 // Methods
 
-import { Methods } from './methods.js'
+import { Methods } from './methods.js';
+let methods = new Methods;
 
-let methods = new Methods
+// Card creation functionality
 
-// Listeners
-
-// opens the modal window
-addCardBtn.addEventListener('click', () => methods.toggle(modal))
-
-// listens to events in modal window
-modal.addEventListener('click', event => {
-    if (event.target.classList.contains('add-card-modal__close-btn')) {
-        methods.toggle(modal) // closes the modal
-
-        setTimeout(() => {
-            methods.clearInput(modal);
-            methods.hideErrorMessage(titleInput, descriptionInput);
-        }, 500); // clears inputs so that there are no previously entered values and hide error messages if they
-                         // were shown
-                         // timeout is needed so that the clearing of values weren't visible during window fade out
-    }
-
-    if (event.target.classList.contains('add-card-modal__save-btn')) { // makes the necessary checks, shows error if empty, else adds a card
+saveBtn.addEventListener('click', event => {
         if (methods.checkIfEmpty(titleInput, descriptionInput)) {
+            return
         } else {
             let cardObject = {
                 id: methods.generateID(),
@@ -52,12 +36,6 @@ modal.addEventListener('click', event => {
             methods.clampText();
             methods.toggle(modal);
         }
-    }
 });
 
-modal.addEventListener ('keyup', event => { // removes error message when user starts to type into the input
-    if (event.target.classList.contains('add-card-modal__title-input')
-    || event.target.classList.contains('add-card-modal__description-input')) {
-        methods.hideErrorMessage(event.target)
-    }
-});
+
