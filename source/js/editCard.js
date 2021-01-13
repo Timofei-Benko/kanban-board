34a4d09aed9f13080b57dbe023187363c8aed9ff
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-01-14 00:21:40 +0300

    fix card editting

--- source/js/editCard.js
+++ source/js/editCard.js
@@ -5,10 +5,20 @@ import { variables } from "./variables.js";
 
 let methods = new Methods();
 
+const   columns = document.body.querySelectorAll('.board__column');
+const   lastColumn = columns[columns.length - 1].dataset.column;
+
 let currentTitleValue,
     currentDescriptionValue;
 
 variables.editModal.addEventListener('click', event => {
+
+    let     cardId = variables.editModal.dataset.edit_card_id,
+            card = document.body.querySelector(`div[data-card_id="${cardId}"]`),
+            column = card.closest('div[data-column]').dataset.column,
+            cardTitleUI = card.querySelector('.board__card-title'),
+            cardDescriptionUI = card.querySelector('.board__card-copy')
+
 
     if (event.target.classList.contains('modal__save-title-btn') ||
         event.target.closest('.modal__save-title-btn')) {
@@ -50,13 +60,7 @@ variables.editModal.addEventListener('dblclick', event => {
             card = document.body.querySelector(`div[data-card_id="${cardId}"]`),
             column = card.closest('div[data-column]').dataset.column;
 
-    const   board = document.body.querySelector('.board__column-container');
-    const   lastColumn = board.children[board.children.length - 1].dataset.column;
-
     if (column === lastColumn) {
-        // TODO:
-        //  - figure out how to remove hover class in done
-        variables.titleEM.classList.remove('.modal__element-hover');
         return
     }
 
