56c2ad7cbc716f8015c495b95625e7705b6a7471
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-01-14 00:22:21 +0300

    fix: modal display && feat: remove hover class in last column

--- source/js/modalDisplay.js
+++ source/js/modalDisplay.js
@@ -8,14 +8,17 @@ const   editCardModal = document.body.querySelector('#edit-card-modal'),
         addCardModal = document.body.querySelector('#add-card-modal'),
         modalWindows = document.body.querySelectorAll('.modal'),
         cardsContainers = document.body.querySelectorAll('.board__cards-container'),
-        addCardBtn = document.body.querySelector('.board__add-new-btn');
+        addCardBtn = document.body.querySelector('.board__add-new-btn'),
+        columns = document.body.querySelectorAll('.board__column'),
+        lastColumn = columns[columns.length - 1].dataset.column;
 
-// Show modal window
+// Open modal window
 
 addCardBtn.addEventListener('click', () => methods.toggle(addCardModal))
 
 for (let i = 0; i < cardsContainers.length; i++) {
     cardsContainers[i].addEventListener('click', event => {
+        let column = event.target.closest('div[data-column]').dataset.column
         if (event.target.classList.contains('board__card-remove-btn') ||
             event.target.closest('.board__card-remove-btn') ||
             event.target.classList.contains('board__card-move-btn') ||
@@ -23,9 +26,20 @@ for (let i = 0; i < cardsContainers.length; i++) {
             return
         } else if (event.target.classList.contains('.board__card') ||
             event.target.closest('.board__card')) {
+
+            if (column === lastColumn) {
+                variables.titleEM.classList.remove('modal__element-hover');
+                variables.descriptionEM.classList.remove('modal__element-hover');
+            } else if (column !== lastColumn &&
+                       !variables.titleEM.classList.contains('modal__element-hover') ||
+                       !variables.descriptionEM.classList.contains('modal__element-hover')){
+                variables.titleEM.classList.add('modal__element-hover');
+                variables.descriptionEM.classList.add('modal__element-hover')
+
+            }
+
             let card = event.target.closest('.board__card'),
                 cardId = card.dataset.card_id,
-                column = event.target.closest('div[data-column]').dataset.column,
                 cardObject = methods.getCardFromLocalStorage(column, cardId);
 
             variables.editModal.dataset.edit_card_id = card.dataset.card_id
